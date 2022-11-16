c79619b41bae3c25efd24f6ee45bd634384f7914
Jon Seager <jon@sgrs.uk>
2022-11-16 15:40:25 +0000

    tests: fix the broken unit test with pebble connections

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,3 @@
-# Copyright 2022 Canonical Ltd.
+# Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
-
-import ops.testing
-
-ops.testing.SIMULATE_CAN_CONNECT = True
+ZINC = "zinc"
