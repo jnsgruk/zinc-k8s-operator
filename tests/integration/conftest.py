e894f3bdce1841d6e62d01a67527eb3081009e01
Jack Shaw <jack.shaw@canonical.com>
2022-11-16 14:59:37 +0000

    Update tests/integration/conftest.py
    Co-authored-by: Jon Seager <jon@sgrs.uk>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -16,21 +16,15 @@ logger = logging.getLogger(__name__)
 async def zinc_deploy_kwargs(ops_test: OpsTest, request):
     zinc_metadata = yaml.safe_load(Path("./metadata.yaml").read_text())
     zinc_channel = request.config.getoption("channel", default=None)
-    build_from_source = zinc_channel is None
-    if build_from_source:
+    # Set some deploy args that are consistent irrespective of charm src
+    args = {"application_name": ZINC, "trust": True}
+
+    if zinc_channel is None:
         charm_path = await ops_test.build_charm(".")
         zinc_oci_image = zinc_metadata["resources"]["zinc-image"]["upstream-source"]
-        return {
-            "entity_url": charm_path,
-            "application_name": ZINC,
-            "resources": {"zinc-image": zinc_oci_image},
-            "trust": True,
-        }
+        args.update({"entity_url": charm_path, "resources": {"zinc-image": zinc_oci_image}})
     else:
-        logger.info(f"Using zinc from charmhub with channel {zinc_channel}")
-        return {
-            "entity_url": "ch:zinc-k8s",
-            "application_name": ZINC,
-            "channel": zinc_channel,
-            "trust": True,
-        }
+        logger.info("Deploying zinc from charmhub with channel %s", zinc_channel)
+        args.update({"entity_url": "zinc-k8s", "channel": zinc_channel})
+
+    return args
