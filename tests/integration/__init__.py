4595c794974ece84612f618e4c641be85a80b9fb
Jon Seager <jon@sgrs.uk>
2025-03-24 18:44:29 +0000

    test: ensure `retry` decorator doesn't mask exceptions

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -12,13 +12,16 @@ def retry(retry_num, retry_sleep_sec):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            for _ in range(retry_num):
+            for i in range(retry_num):
                 try:
                     return func(*args, **kwargs)
-                except Exception:
+                except Exception as exc:
+                    if i >= retry_num - 1:
+                        raise Exception(f"Exceeded {retry_num} retries") from exc
+                    logging.error(
+                        "func %s failure %d/%d: %s", func.__name__, i + 1, retry_num, exc
+                    )
                     time.sleep(retry_sleep_sec)
-            logging.error("func %s retry failed", func)
-            raise Exception("Exceed max retry num: {} failed".format(retry_num))
 
         return wrapper
 
